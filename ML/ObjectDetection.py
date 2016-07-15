581775945cf9f2da25531ff9304b3bdaeb4f43cb
Faiz Siddiqui <faize.sid@gmail.com>
2016-07-16 02:43:07 +0530

    Palm Classifier added.

--- ML/ObjectDetection.py
+++ ML/ObjectDetection.py
@@ -23,3 +23,15 @@ class ObjectDetection(object):
 			roi = gray_image[y_pos+10:y_pos + height-10, x_pos+10:x_pos + width-10]
 
 		return y
+
+	def detectMultiScale(elf, cascade_classifier, gray_image, image):
+		y = 0
+		cascade_obj = cascade_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=2)
+		for (x_pos, y_pos, width, height) in cascade_obj:
+			cv2.rectangle(image, (x_pos+5, y_pos+5), (x_pos+width-5, y_pos+height-5), (255, 255, 255), 2)
+			y = y_pos + height - 5
+
+			#perform more action inside the detected object
+			roi = gray_image[y_pos+10:y_pos + height-10, x_pos+10:x_pos + width-10]
+
+		return cascade_obj
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import threading
 import time
 import json
 
-from Camera import IPCamera as camera
 from ML import ObjectDetection as od
 from Socket.Server import Server
 from IOT.REST import REST
@@ -82,11 +81,14 @@ class Threads:
     def thread_ObjectDetection(config):
         obj_detection = od.ObjectDetection()
         face_cascade = cv2.CascadeClassifier('ML/cascade_xml/haarcascade_frontalface_default.xml')
+        palm_cascade = cv2.CascadeClassifier('ML/cascade_xml/palm.xml')
 
         if config["camera"]["status"] == "IP":
+            from Camera import IPCamera as camera
             cam = camera.IPCamera(config["camera"]["IP"]["host"] + ":" + str(config["camera"]["IP"]["port"]) + config["camera"]["IP"]["url"])
         else:
-            cam = Camera()
+            from Camera import BoardCamera as camera
+            cam = camera.BoardCamera()
         try:
             while True:
                 original = cam.get_frame()
@@ -95,7 +97,10 @@ class Threads:
                     os.system("pause")
                     break
                 gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-                v_param1 = obj_detection.detect(face_cascade, gray, original)
+                v_param1 = obj_detection.detectMultiScale(face_cascade, gray, original)
+                v_param2 = obj_detection.detectMultiScale(palm_cascade, gray, original)
+                print "Found {0} faces!".format(len(v_param1))
+                print "Found {0} palm!".format(len(v_param2))
 
                 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
                 cv2.imshow('output',original)
@@ -110,14 +115,14 @@ class Threads:
         finally:
             print "Connection closed on Object detection thread."
 
-    ultrasonic_thread = threading.Thread(target=thread_ultrasonic(config))
+    '''ultrasonic_thread = threading.Thread(target=thread_ultrasonic(config))
     ultrasonic_thread.start()
 
     IR_thread = threading.Thread(target=thread_IR(config, rest))
     IR_thread.start()
 
     GPS_thread = threading.Thread(target = thread_GPS(config, rest))
-    GPS_thread.start()
+	GPS_thread.start()'''
 
     object_detection_thread = threading.Thread(target = thread_ObjectDetection(config))
     object_detection_thread.start()
